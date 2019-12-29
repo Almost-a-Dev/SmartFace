82581f1681b63d1e6ad5370057d66f54c51501b3
Alejandro Olmos <manuelalejandro.soto1@gmail.com>
2019-12-29 15:02:02 -0300

    Add logic to register and signin component

--- src/App.js
+++ src/App.js
@@ -3,18 +3,43 @@ import React, { Component } from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Register from './components/Register/Register';
 import Signin from './components/Signin/Signin';
+import SmartFace from './components/SmartFace/SmartFace';
 import './App.css';
 
-
+const initState = {
+  route: 'signin',
+  isSigned: false
+}
 
 class App extends Component {
 
+  constructor() {
+    super();
+    this.state = initState;
+  }
+
+  urlChange = (route) => {
+    if (route === 'signout') {
+      this.setState(initState)
+    } else if (route === 'home') {
+      this.setState({isSigned: true})
+    }
+    this.setState({route: route});
+  }
+
   render() {
+    const { isSigned, route } = this.state;
     return (
     <div className="App">
-      <Navbar />
-      <Register />
-      <Signin />
+      <Navbar isSigned={isSigned} urlChange={this.urlChange}/>
+      { route === 'home'
+        ? <SmartFace />
+        : (
+          route === 'signin'
+            ? <Signin urlChange={this.urlChange}/>
+            : <Register urlChange={this.urlChange}/>
+        )
+      }
     </div>
     );
   }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
-const Navbar = () => {
-    return (
+const Navbar = ({ urlChange, isSigned }) => {
+    if (isSigned) {
+      return (
         <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-            <p className='f3 link dim black underline pa3 pointer'>Register</p>
-            <p className='f3 link dim black underline pa3 pointer'>Sign In</p>
-            <p className='f3 link dim black underline pa3 pointer'>Sign Out</p>
+          <p onClick={() => urlChange('signout')} className='f3 link dim black underline pa3 pointer'>Sign Out</p>
         </nav>
-    );
+      );
+    } else {
+      return (
+        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+          <p onClick={() => urlChange('signin')} className='f3 link dim black underline pa3 pointer'>Sign In</p>
+          <p onClick={() => urlChange('register')} className='f3 link dim black underline pa3 pointer'>Register</p>
+        </nav>
+      );
+    }
 }
 
 export default Navbar;
