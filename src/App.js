fac3ddc95677fed861ab17b894f9f0036a248c12
Alejandro Olmos <manuelalejandro.soto1@gmail.com>
2019-12-30 09:42:36 -0300

    Add ImageForm component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import Navbar from './components/Navbar/Navbar';
 import Register from './components/Register/Register';
 import Signin from './components/Signin/Signin';
 import SmartFace from './components/SmartFace/SmartFace';
+import ImageForm from './components/ImageForm/ImageForm';
 import './App.css';
 
+//Default App state
 const initState = {
   route: 'signin',
-  isSigned: false
+  isSigned: false,
+  input: ''
 }
 
 class App extends Component {
@@ -18,6 +21,7 @@ class App extends Component {
     this.state = initState;
   }
 
+  //Harcoded routing react, will be changed later, just for test...
   urlChange = (route) => {
     if (route === 'signout') {
       this.setState(initState)
@@ -27,13 +31,29 @@ class App extends Component {
     this.setState({route: route});
   }
 
+  //catch the input from the input of ImageForm component...
+  catchInput = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  //Submiting a image for the API, this will be code later, it's just to test that works for now :)
+  submitImage = () => {
+    console.log(`I'm submit and my state is ${this.state.input}`);
+  }
+
   render() {
     const { isSigned, route } = this.state;
     return (
     <div className="App">
       <Navbar isSigned={isSigned} urlChange={this.urlChange}/>
       { route === 'home'
-        ? <SmartFace />
+        ? <div>
+          <ImageForm 
+            catchInput={this.catchInput}
+            submitImage={this.submitImage}
+          />
+          <SmartFace input={this.state.input}/>
+        </div> 
         : (
           route === 'signin'
             ? <Signin urlChange={this.urlChange}/>
--- src/components/SmartFace/SmartFace.js
+++ src/components/SmartFace/SmartFace.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import './SmartFace.css';
 
-const SmartFace = () => {
+const SmartFace = ({input}) => {
     return (
-        <div>
-            <h1>Main component!</h1>
+      <div className='center ma'>
+        <div className='absolute mt2'>
+          <img id='inputimage' alt='' width='500px' heigh='auto'/>
+          <div className='facebox'><h1>{input}</h1></div>
         </div>
+      </div>
     );
-}
+  }
 
 export default SmartFace;
