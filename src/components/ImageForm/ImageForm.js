2be9852603af37368a8bb347cf6dada786ae217e
Alejandro Olmos <manuelalejandro.soto1@gmail.com>
2020-01-01 10:47:18 -0300

    minimal changes at css styles

--- src/components/ImageForm/ImageForm.js
+++ src/components/ImageForm/ImageForm.js
@@ -10,7 +10,7 @@ const ImageForm = ({ catchInput, submitImage }) => {
         <div className='form center pa4 br3 shadow-5'>
           <input className='f4 pa2 w-70 center' type='tex' onChange={catchInput}/>
           <button
-            className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
+            className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple mt4'
             onClick={submitImage}
           >Check Image!</button>
         </div>
